4fea2529151f320648d3bd83f573517a11548143
yazhiniii20 <160877024+yazhiniii20@users.noreply.github.com>
2026-04-13 22:51:45 +0530

    feat: add movie details view with API integration and conditional rendering

--- movie-search-app-react/src/MovieSearch.js
+++ movie-search-app-react/src/MovieSearch.js
@@ -5,6 +5,7 @@ function MovieSearch(){
     const [movies , setMovies] = useState([]);
     const [loading, setLoading] = useState(false);
     const [hasSearched, setHasSearched] = useState(false);
+    const [selectedMovie , setselectedMovie] = useState([]);
     async function getMovies(search){
         setHasSearched(true);
         if(search.trim() === ""){
@@ -22,6 +23,16 @@ function MovieSearch(){
         }
         setLoading(false);
     }
+    async function getselectedmoviedetails(imdbID){
+        const response = await fetch(`https://www.omdbapi.com/?apikey=a46aa13b&i=${imdbID}`);
+        const data = await response.json();
+        console.log(data);
+        if(data.Response === "False"){
+            setselectedMovie(null);
+        }else{
+            setselectedMovie(data);
+        }
+    }
     return(
       <div className = "movie-container">
       <input type = "text" value = {search} onChange = {(e) => setSearch(e.target.value)}/>
@@ -32,14 +43,23 @@ function MovieSearch(){
      )}
       <div className = "movie-grid">
         {movies.map(movie => (
-            <div id = {movie.imdbID} className = "movie-card">
+            <div id = {movie.imdbID} className = "movie-card"  onClick = {() => getselectedmoviedetails(movie.imdbID)}>
                 <h3> {movie.Title}</h3>
+                <h4> {movie.Year} </h4>
                 <img src = {movie.Poster} alt = {movie.Title} onError={(e) => {
     e.target.src = "https://placehold.net/400x400.png";
   }}/>
+                   {selectedMovie && (
+            <div>
+                <h2> {selectedMovie.Title} </h2>
+                <p> {selectedMovie.Plot} </p>
             </div>
+        )} 
+            </div>
+            
         ))}
         </div>
+      
       </div>
     );
 }
